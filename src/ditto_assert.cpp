841d588c80cadb3be691050eb5816787245ea419
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2022-08-03 22:26:17 +0200

    Format

--- src/ditto_assert.cpp
+++ src/ditto_assert.cpp
@@ -7,14 +7,12 @@ extern Postform::SerialLogger<Postform::Rtt::Transport> logger;
 
 namespace Ditto {
 void assert_failed(const char* condition, int line, const char* file) {
-  LOG_ERROR(&logger, "Assert `%s` failed at line %d, file %s", condition, line,
-            file);
+  LOG_ERROR(&logger, "Assert `%s` failed at line %d, file %s", condition, line, file);
   CortexMHal::halt();
 }
 
 void unimplemented(const char* function, int line, const char* file) {
-  LOG_ERROR(&logger, "Assert `%s` failed at line %d, file %s", function, line,
-            file);
+  LOG_ERROR(&logger, "Assert `%s` failed at line %d, file %s", function, line, file);
   CortexMHal::halt();
 }
 }  // namespace Ditto
